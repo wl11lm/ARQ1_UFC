73ac88ccd70fcdad1cdf5297f57a8a38c78cb615
William Marreiro <62153578+wl11lm@users.noreply.github.com>
2023-04-11 20:31:14 -0300

    Add files via upload

---
+++ Cap02/questao05.asm
@@ -0,0 +1,23 @@
+    .686
+    .model flat, c
+    .stack 100h
+printf PROTO arg1:Ptr Byte, printlist:VARARG
+    .data
+msg1fmt byte 0Ah,"%d%s%d%s%d",0Ah,0Ah,0
+msg1 byte " + " , 0
+msg2 byte " = " , 0
+x sdword ?
+y sdword ?
+z sdword ?
+    .code
+main proc
+mov eax, 1
+mov x, eax
+mov eax, 2
+mov y, eax
+mov eax, 3
+mov z, eax
+INVOKE printf,ADDR msg1fmt, x, ADDR msg1, y , ADDR msg2, z
+ret
+main endp
+end

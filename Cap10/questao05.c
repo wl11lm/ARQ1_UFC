2d3d00c3f2b7ac07726464b9c72c25bcdd2d31a4
William Marreiro <62153578+wl11lm@users.noreply.github.com>
2023-07-13 19:36:51 -0300

    Create questao05.c

---
+++ Cap10/questao05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+  
+  long double x,y;
+  
+  printf("\n%s", "Enter a double for x: ");
+  scanf_s("%Lf", &x);
+  
+  __asm {
+    fld x
+    fstp y
+  }
+
+  printf("\n%s%6.4Lf\n\n", "The double in y is: ", y);
+
+  return 0;
+}
